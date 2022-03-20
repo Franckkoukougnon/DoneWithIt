2a3afcc0ecd103e9b3af981eea5dbfa5234f62c5
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-03-20 22:55:39 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
+import {
+  Button,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from "react-native-web";
 
 export default function App() {
+  const handlePresse = () => console.log("text pressed !!!");
+
+  console.log(require("./assets/ezy.jpg"));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
+        <Image
+          fadeDuration={300}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/id/237/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableHighlight
+        style={{ margin: 5 }}
+        onPress={() => console.log("Image cliquéé")}
+      >
+        <Button title="click Me" onPress={() => alert("clicked")} />
+      </TouchableHighlight>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 300,
+    height: 150,
   },
 });
